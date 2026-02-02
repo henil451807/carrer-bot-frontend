7ae774539677da5c632e199584e70c9076a3ca07
Prince <malaniprince55@gmail.com>
2026-02-03 00:45:05 +0530

    text related changes

--- src/components/Chat/ChatPage.tsx
+++ src/components/Chat/ChatPage.tsx
@@ -307,7 +307,7 @@ const ChatPage: React.FC = () => {
             >
               ☰
             </button>
-            <h1 className="chat-header__title">Career Guidance Chat</h1>
+            <h1 className="chat-header__title">JYOTI : Your Career & Youth Opportunity Assistant</h1>
             <div className="chat-header__actions">
               <ContactMenu />
               <button
